01f12f9d496d59467e6f39ef82ca11743cc81ae3
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-01-10 10:22:43 -0600

    added hourupdater/adjusted css styles

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,37 @@ setInterval(function(){
     $('#currentDay').text(date);
 }, 1000);
 
+// updates Timeblock color based on the hour
+function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".description").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // add class "past" if currentHour is greater than blockHour
+      if(currentHour > blockHour) {
+        $(this).addClass("past");
+      }
+     
+      //remove class "past" and add class "present" if currentHour = blockHourd
+      else if(currentHour === blockHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+     
+      // remove class "past", remove class "present", add class "future" if time has passed
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+      
+    });
+}
+
+hourUpdater();
 
 
 
